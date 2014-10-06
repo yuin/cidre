9dad30d116b73724670e136e2b27673ca70c4206
yuin <yuin@inforno.net>
2014-10-06 17:37:12 +0900

    apply gofmt

--- _example/wiki/app.go
+++ _example/wiki/app.go
@@ -50,7 +50,7 @@ type Article struct {
 
 func LoadArticle(file string) (*Article, error) {
 	basename := filepath.Base(file)
-    article := &Article{Name: basename[0 : len(basename)-len(".txt")], Body:""}
+	article := &Article{Name: basename[0 : len(basename)-len(".txt")], Body: ""}
 	if body, err := ioutil.ReadFile(file); err != nil {
 		return article, errors.New("Error")
 	} else {
@@ -83,41 +83,41 @@ func main() {
 	appConfig := cidre.DefaultAppConfig()
 	sessionConfig := cidre.DefaultSessionConfig()
 	_, err := cidre.ParseIniFile("app.ini",
-        // cidre
+		// cidre
 		cidre.ConfigMapping{"cidre", appConfig},
-        // session middleware
+		// session middleware
 		cidre.ConfigMapping{"session.base", sessionConfig},
-        // this app
+		// this app
 		cidre.ConfigMapping{"wiki", wikiConfig},
 	)
 	if err != nil {
 		panic(err)
 	}
-    // Renderer configuration & view helper functions
-    renderConfig := cidre.DefaultHtmlTemplateRendererConfig()
-    renderConfig.TemplateDirectory = appConfig.TemplateDirectory
-    renderConfig.FuncMap["nl2br"] = func(text string) template.HTML {
-      return template.HTML(strings.Replace(text, "\n", "<br />", -1))
-    }
+	// Renderer configuration & view helper functions
+	renderConfig := cidre.DefaultHtmlTemplateRendererConfig()
+	renderConfig.TemplateDirectory = appConfig.TemplateDirectory
+	renderConfig.FuncMap["nl2br"] = func(text string) template.HTML {
+		return template.HTML(strings.Replace(text, "\n", "<br />", -1))
+	}
 
 	app := cidre.NewApp(appConfig)
-    // Set our HTML renderer 
-    app.Renderer = cidre.NewHtmlTemplateRenderer(renderConfig)
-    // Use the session middleware for flash messaging
+	// Set our HTML renderer
+	app.Renderer = cidre.NewHtmlTemplateRenderer(renderConfig)
+	// Use the session middleware for flash messaging
 	app.Use(cidre.NewSessionMiddleware(app, sessionConfig, nil))
 	root := app.MountPoint("/")
 
-    // serve static files
+	// serve static files
 	root.Static("statics", "statics", "./statics")
 
 	root.Get("show_pages", "", func(w http.ResponseWriter, r *http.Request) {
 		files, err := filepath.Glob(filepath.Join(wikiConfig.DataDirectory, "*.txt"))
-        if err != nil {
-          app.OnPanic(w, r, err)
-        }
+		if err != nil {
+			app.OnPanic(w, r, err)
+		}
 		articles := make(Articles, 0, len(files))
 		for _, file := range files {
-            article, _ := LoadArticle(file)
+			article, _ := LoadArticle(file)
 			articles = append(articles, article)
 		}
 		sort.Sort(articles)
@@ -167,13 +167,13 @@ func main() {
 		ctx := cidre.RequestContext(r)
 		name := strings.Replace(ctx.PathParams.Get("name"), "..", "", -1)
 		file := filepath.Join(wikiConfig.DataDirectory, name+".txt")
-        if err := os.Remove(file); err != nil {
-          app.OnPanic(w, r, err)
-          return
-        }
+		if err := os.Remove(file); err != nil {
+			app.OnPanic(w, r, err)
+			return
+		}
 		ctx.Session.AddFlash("info", "Page deleted")
 		http.Redirect(w, r, app.BuildUrl("show_pages"), http.StatusFound)
-    })
+	})
 
 	app.Hooks.Add("start_request", func(w http.ResponseWriter, r *http.Request, data interface{}) {
 		w.Header().Add("X-Server", "Go")
